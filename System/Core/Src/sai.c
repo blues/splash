528b1c24e946e5efcc3dec587849aafc45bad343
Ray Ozzie <ray@ozzie.net>
2025-01-25 10:27:37 -0500

    non-circular sai

--- System/Core/Src/sai.c
+++ System/Core/Src/sai.c
@@ -100,7 +100,7 @@ void HAL_SAI_MspInit(SAI_HandleTypeDef* hsai)
         hdma_sai1_a.Init.MemInc = DMA_MINC_ENABLE;
         hdma_sai1_a.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
         hdma_sai1_a.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-        hdma_sai1_a.Init.Mode = DMA_CIRCULAR;
+        hdma_sai1_a.Init.Mode = DMA_NORMAL;
         hdma_sai1_a.Init.Priority = DMA_PRIORITY_HIGH;
         HAL_DMA_Init(&hdma_sai1_a);
 
